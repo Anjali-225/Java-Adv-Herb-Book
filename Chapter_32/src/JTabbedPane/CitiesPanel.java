33bebf389f4314ea57898a593090e3769c4cc9d7
Anjali <anjali.payal69@gmail.com>
2021-01-22 08:52:58 +0200

    Exploring Swing

---
+++ Chapter_32/src/JTabbedPane/CitiesPanel.java
@@ -0,0 +1,19 @@
+package JTabbedPane;
+
+import javax.swing.*;
+
+// Make the panels that will be added to the tabbed pane.
+
+public class CitiesPanel extends JPanel {
+
+    public CitiesPanel() {
+        JButton b1 = new JButton("New York");
+        add(b1);
+        JButton b2 = new JButton("London");
+        add(b2);
+        JButton b3 = new JButton("Hong Kong");
+        add(b3);
+        JButton b4 = new JButton("Tokyo");
+        add(b4);
+    }
+}
