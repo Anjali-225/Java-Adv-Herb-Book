7286782f8e97594efc7e36e1c55d640ede7d14eb
Anjali <anjali.payal69@gmail.com>
2021-01-22 11:16:05 +0200

    Java Beans

--- Chapter_34/src/Introspection/SimpleProperties.java
+++ Chapter_34/src/Introspection/SimpleProperties.java
@@ -1,4 +1,30 @@
 package Introspection;
 
 public class SimpleProperties {
+
+    private double depth, height, width;
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double d) {
+        depth = d;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double h) {
+        height = h;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double w) {
+        width = w;
+    }
 }
